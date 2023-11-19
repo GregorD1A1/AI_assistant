0a4c7cf4d1c730323830a44b9634c68c8a819faf
GregorD1A1 <dudnikgrv@gmail.com>
2023-11-19 17:23:49 +0100

    req

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-
-@app.route('/', methods=['GET', 'POST'])
-def endpoint():
-    data = request.get_json()  # Get the JSON data from the request
-    print(data)
-
-    # Perform any data processing or logic here
-
-    response = {'message': 'Success', 'data': data}  # Create a response JSON
-
-    return jsonify(response)  # Return the response as JSON
-
-
-if __name__ == '__main__':
-    app.run()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
