199b3bf674116bdd37da98ba015835f18e49c7b4
GregorD1A1 <dudnikgrv@gmail.com>
2023-12-07 13:47:41 +0100

    dd

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, request
 from conv_flow import conversate
 import sys
+import logging
 
 app = Flask(__name__)
 
-
+logging.basicConfig(level=logging.INFO)
 @app.route('/', methods=['POST'])
 def execute():
     sys.stdout.write("Request received.\n")
+    logging.info('Dzik!')
     body = request.get_json()
     message = body['message']
     conversate(message)
